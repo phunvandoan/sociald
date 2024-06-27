8abaf59c43c3f6829c1c9562925b07b8888c4f97
PHUNG VAN DOAN <phungvandoan2k3@gmail.com>
2024-06-27 10:22:17 +0700

    a

--- src/pages/messenger/Messenger.js
+++ src/pages/messenger/Messenger.js
@@ -20,7 +20,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:8900");
+    socket.current = io("https://socket-of-social-d.vercel.app/");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
