5a5fa30b92c1e99b810701df041cd94f898965f0
PHUNG VAN DOAN <phungvandoan2k3@gmail.com>
2024-06-27 11:06:55 +0700

    updated today 1

--- src/pages/messenger/Messenger.js
+++ src/pages/messenger/Messenger.js
@@ -20,7 +20,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("https://socketofsociald.onrender.com/");
+    socket.current = io("ws://socketofsociald.onrender.com/");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
